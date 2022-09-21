0284959348b26f15a9f280c53511e5058e459d7b
isabelwiner <112510768+isabelwiner@users.noreply.github.com>
2022-09-21 13:07:15 -0700

    h

--- sketch_sep14a/sketch_sep14a.ino
+++ sketch_sep14a/sketch_sep14a.ino
@@ -1,42 +1,13 @@
-/*
-  This code should work to get warning cross the buzzer when something be closer than 0.5 meter
-  Circuit is ultrasonic sensor and buzzer +5v and Arduino uno is used
-  a_atef45@yahoo.com
-  www.zerosnones.net
-  +201153300223
-*/
-// Define pins for ultrasonic and buzzer
-int const trigPin = 10;
-int const echoPin = 9;
-int const buzzPin = 2;
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-void setup()
-{
-  pinMode(trigPin, OUTPUT); // trig pin will have pulses output
-  pinMode(echoPin, INPUT); // echo pin should be input to get pulse width
-  pinMode(buzzPin, OUTPUT); // buzz pin is output to control buzzering
+void setup() {
+  lcd.begin(16,2);
+  lcd.print("hi mom and dad!");
 }
 
-void loop()
-{
-  // Duration will be the input pulse width and distance will be the distance to the obstacle in centimeters
-  int duration, distance;
-  // Output pulse with 1ms width on trigPin
-  digitalWrite(trigPin, HIGH); 
-  delay(1);
-  digitalWrite(trigPin, LOW);
-  // Measure the pulse input in echo pin
-  duration = pulseIn(echoPin, HIGH);
-  // Distance is half the duration devided by 29.1 (from datasheet)
-  distance = (duration/2) / 29.1;
-  // if distance less than 0.5 meter and more than 0 (0 or less means over range) 
-    if (distance <= 50 && distance >= 0) {
-      // Buzz
-      digitalWrite(buzzPin, HIGH);
-    } else {
-      // Don't buzz
-      digitalWrite(buzzPin, LOW);
-    }
-    // Waiting 60 ms won't hurt any one
-    delay(60);
+void loop() {
+  lcd.setCursor(0,1);
+  lcd.print(millis() / 1000);
 }
