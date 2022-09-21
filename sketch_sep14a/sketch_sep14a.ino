4ba14d8e53bdf2d260284c1a7cc369abb86d5649
isabelwiner <112510768+isabelwiner@users.noreply.github.com>
2022-09-21 12:37:52 -0700

    hi

--- sketch_sep14a/sketch_sep14a.ino
+++ sketch_sep14a/sketch_sep14a.ino
@@ -1,64 +1,42 @@
 /*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
-
+  This code should work to get warning cross the buzzer when something be closer than 0.5 meter
+  Circuit is ultrasonic sensor and buzzer +5v and Arduino uno is used
+  a_atef45@yahoo.com
+  www.zerosnones.net
+  +201153300223
 */
+// Define pins for ultrasonic and buzzer
+int const trigPin = 10;
+int const echoPin = 9;
+int const buzzPin = 2;
 
-// include the library code:
-#include <LiquidCrystal.h>
-
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
-void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+void setup()
+{
+  pinMode(trigPin, OUTPUT); // trig pin will have pulses output
+  pinMode(echoPin, INPUT); // echo pin should be input to get pulse width
+  pinMode(buzzPin, OUTPUT); // buzz pin is output to control buzzering
 }
 
-void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+void loop()
+{
+  // Duration will be the input pulse width and distance will be the distance to the obstacle in centimeters
+  int duration, distance;
+  // Output pulse with 1ms width on trigPin
+  digitalWrite(trigPin, HIGH); 
+  delay(1);
+  digitalWrite(trigPin, LOW);
+  // Measure the pulse input in echo pin
+  duration = pulseIn(echoPin, HIGH);
+  // Distance is half the duration devided by 29.1 (from datasheet)
+  distance = (duration/2) / 29.1;
+  // if distance less than 0.5 meter and more than 0 (0 or less means over range) 
+    if (distance <= 50 && distance >= 0) {
+      // Buzz
+      digitalWrite(buzzPin, HIGH);
+    } else {
+      // Don't buzz
+      digitalWrite(buzzPin, LOW);
+    }
+    // Waiting 60 ms won't hurt any one
+    delay(60);
 }
